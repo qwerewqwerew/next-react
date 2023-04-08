5e08b5d832ca6171999e3151dc52e60851a0d1df
qwerewqwerew <qwerew0@gmail.com>
2023-04-08 23:32:16 +0900

    03-03css추가

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>2023년 4월 2주차</div>
+      <div>
+        <h2>자동차보험</h2>
+        <div>₩294,000</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,10 @@
+import React, {} from 'react'
+import "./ExpenseItem.css";
+const ExpensItem = () => {
+  return (
+    <h2>
+      ExpensItem
+    </h2>
+  )
+}
+export default ExpensItem;
