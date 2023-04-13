108b7e824df5dccd99c5491582146f67fd1a1d05
qwerewqwerew <qwerew0@gmail.com>
2023-04-14 00:06:44 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-function ExpenseItem() {
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: '화장지',
+      amount: 9400,
+      date: new Date(2023, 7, 14),
+    },
+    { id: 'e2', title: '모니터', amount: 200000, date: new Date(2023, 2, 12) },
+    {
+      id: 'e3',
+      title: '자동차보험',
+      amount: 294000,
+      date: new Date(2023, 4, 1),
+    },
+    {
+      id: 'e4',
+      title: '밥상(스뎅)',
+      amount: 4500,
+      date: new Date(2023, 5, 12),
+    },
+  ];
   return (
     <div>
-      <div>2023년 4월 2주차</div>
-      <div>
-        <h2>자동차보험</h2>
-        <div>₩294,000</div>
-      </div>
+      <h1>팔자피자앱</h1>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
     </div>
   );
 }
 
-export default ExpenseItem;
+export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,20 @@
-import React, {} from 'react'
-import "./ExpenseItem.css";
-const ExpensItem = () => {
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+  console.log(props);
+  const expenseDate = new Date(2023, 2, 28);
+  const expenseTitle = "자동차보험";
+  const expenseAmount = 290000;
+
   return (
-    <h2>
-      ExpensItem
-    </h2>
-  )
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">₩{props.amount}</div>
+      </div>
+    </div>
+  );
 }
-export default ExpensItem;
+
+export default ExpenseItem;
