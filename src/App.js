9a9c7f3f52cec5d56b384da84d168dc8cc17f94e
qwerewqwerew <qwerew0@gmail.com>
2023-04-21 00:09:33 +0900

    splitting components

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
   ];
   return (
     <div>
-      <h1>팔자피자앱</h1>
+      <h1>머치모아앱</h1>
       <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
       <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
       <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,16 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  console.log(props);
-  const expenseDate = new Date(2023, 2, 28);
-  const expenseTitle = "자동차보험";
-  const expenseAmount = 290000;
-
-  return (
-    <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">₩{props.amount}</div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="expense-item">
+			<ExpenseDate date={props.date}/>
+			<div className="expense-item__description">
+				<h2>{props.title}</h2>
+				<div className="expense-item__price">₩{props.amount}</div>
+			</div>
+		</div>
+	);
 }
 
 export default ExpenseItem;
