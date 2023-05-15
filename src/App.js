4ffb3948b773a099702bd17f4695ec44183f9a8b
qwerewqwerew <qwerew0@gmail.com>
2023-05-15 22:26:28 +0900

    09-state작성

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
 import React from "react";
 import Expenses from "./components/Expenses/Expenses";
-import NewExpense from "./components/NewExpense/NewExpense";
 function App() {
-  const addExpenseFn = (expense) => {
-    console.log(expense);
-  }
   const expenses = [
     {
       id: 'e1',
       title: '화장지',
       amount: 9400,
-      date: new Date(2023, 7, 14),
+      date: new Date(2019, 7, 14),
     },
-    {id: 'e2', title: '모니터', amount: 200000, date: new Date(2023, 2, 12)},
+    { id: 'e2', title: '모니터', amount: 200000, date: new Date(2021, 2, 12) },
     {
       id: 'e3',
       title: '자동차보험',
       amount: 294000,
-      date: new Date(2023, 4, 1),
+      date: new Date(2020, 4, 1),
     },
     {
       id: 'e4',
       title: '밥상(스뎅)',
       amount: 4500,
-      date: new Date(2023, 5, 12),
+      date: new Date(2022, 5, 12),
     },
   ];
+  /*
+  return React.createElement(
+    'div', {}, React.createElement('h2', {}, "시작"), React.createElement(Expenses, { items: expenses })
+  );
 
-  return (
-    <div>
-      <h1>머치모아앱</h1>
-      <NewExpense onAddExpense={addExpenseFn} />
-      <Expenses items={expenses} />
-    </div>)
+ */
+  return (<div>
+    <h1>가계부</h1>
+    <Expenses items={expenses} />
+  </div>)
 }
 export default App;
