b315caa9903961d90c9b250cef0ec2859f5f559e
qwerewqwerew <qwerew0@gmail.com>
2023-05-15 20:27:51 +0900

    date 수정

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React from "react";
 import Expenses from "./components/Expenses";
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: '화장지',
-      amount: 9400,
-      date: new Date(2023, 7, 14),
-    },
-    { id: 'e2', title: '모니터', amount: 200000, date: new Date(2023, 2, 12) },
-    {
-      id: 'e3',
-      title: '자동차보험',
-      amount: 294000,
-      date: new Date(2023, 4, 1),
-    },
-    {
-      id: 'e4',
-      title: '밥상(스뎅)',
-      amount: 4500,
-      date: new Date(2023, 5, 12),
-    },
-  ];
-  /*
+	const expenses = [
+		{
+			id: "e1",title: "화장지",
+			amount: 9400,
+			date: new Date(2023, 7, 14),
+		},
+		{ id: "e2", title: "모니터", amount: 200000, date: new Date(2023, 12, 12) },
+		{
+			id: "e3",
+			title: "자동차보험",
+			amount: 294000,
+			date: new Date(2023, 4, 1),
+		},
+		{
+			id: "e4",
+			title: "밥상(스뎅)",
+			amount: 4500,
+			date: new Date(2023, 5, 12),
+		},
+	];
+	/*
   return React.createElement(
     'div', {}, React.createElement('h2', {}, "시작"), React.createElement(Expenses, { items: expenses })
 
@@ -30,9 +29,11 @@ function App() {
   );
 
  */
-  return (<div>
-    <h1>머치모아앱</h1>
-    <Expenses items={expenses} />
-  </div>)
+	return (
+		<div>
+			<h1>가계부</h1>
+			<Expenses items={expenses} />
+		</div>
+	);
 }
 export default App;
--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./ExpenceDate.css";
 const ExpenseDate = (props) => {
-  const month = props.date.toLocaleString("ko-KR", { month: "long" });
+  const month = props.date.getMonth().toString().padStart(2,"0");
   const day = props.date.toLocaleString("ko-KR", { day: "2-digit" });
   const year = props.date.getFullYear();
   return (
     <div className="expense-date">
-      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__month">{month == 0 ? "12" : month}월</div>
       <div className="expense-date__year">{year}</div>
       <div className="expense-date__day">{day}</div>
     </div>
