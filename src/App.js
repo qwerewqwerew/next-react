5be7b26c440c73d36ffecce9145e3a915a8565b2
qwerewqwerew <qwerew0@gmail.com>
2023-05-17 13:46:00 +0900

    map

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
+const INIT_DATA = [
+	{ id: "e1", title: "화장지", amount: 9400, date: new Date(2019, 7, 14) },
+	{ id: "e2", title: "모니터", amount: 200000, date: new Date(2021, 2, 12) },
+	{ id: "e3", title: "자동차보험", amount: 294000, date: new Date(2020, 4, 1) },
+	{ id: "e4", title: "밥상(스뎅)", amount: 4500, date: new Date(2022, 5, 12) },
+];
 function App() {
-	const INIT_DATA = [
-		{ id: "e1", title: "화장지", amount: 9400, date: new Date(2019, 7, 14) },
-		{ id: "e2", title: "모니터", amount: 200000, date: new Date(2021, 2, 12) },
-		{ id: "e3", title: "자동차보험", amount: 294000, date: new Date(2020, 4, 1) },
-		{ id: "e4", title: "밥상(스뎅)", amount: 4500, date: new Date(2022, 5, 12) },
-	];
 	const [expenseList, setExpenseList] = React.useState(INIT_DATA);
+
 	const addExpenseFn = (data) => {
-		console.log(data);
-		return setExpenseList((list) => {
-			return [...list, data];
+		return setExpenseList((prevExpenses) => {
+			return [data, ...prevExpenses];
 		});
 	};
 
@@ -21,7 +21,7 @@ function App() {
 			<h1>가계부</h1>
 			<NewExpense onAddExpense={addExpenseFn} />
 			{/* <Expenses items={expenses} /> */}
-			<Expenses items={[...INIT_DATA, ...expenseList]} />
+			<Expenses items={expenseList} />
 		</div>
 	);
 }
