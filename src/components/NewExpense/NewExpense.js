481d57c7b21e0c4c8433ce4a936ce7c150bc68ba
qwerewqwerew <qwerew0@gmail.com>
2023-05-18 19:19:05 +0900

    condition add

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
-import './NewExpense.css'
+import "./NewExpense.css";
 const NewExpense = (props) => {
-
-  const saveExpenseFn = (data) => {
-    const expenseData = {...data, id: Math.random().toString()};
-    props.onAddExpense(expenseData)
-  }
-  return (
-    <div className="new-expense">
-      <ExpenseForm onSaveExpense={saveExpenseFn} />
-    </div>
-  )
-}
+	const [isAdding, setIsAdding] = useState(false);
+	const addingFn = () => {
+		setIsAdding(true);
+	};
+	const saveExpenseFn = (data) => {
+		const expenseData = { ...data, id: Math.random().toString() };
+		props.onAddExpense(expenseData);
+	};
+	const addCancelfn = () => {
+		setIsAdding(false);
+	};
+	return (
+		<div className='new-expense'>
+			{!isAdding && <button onClick={addingFn}>지출추가</button>}
+			{isAdding && <ExpenseForm onSaveExpense={saveExpenseFn} onCancel={addCancelfn} />}
+		</div>
+	);
+};
 export default NewExpense;
